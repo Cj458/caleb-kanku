6f7ea7cb1a79c47c64f902389871698a5e98dc57
Cj458 <Calebjason90@gmail.com>
2022-01-07 02:17:31 +0300

    here

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,14 +124,14 @@ STATIC_URL = '/staticfiles/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
- ]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles')
+#  ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Activate Django-Heroku.
 django_heroku.settings(locals())
